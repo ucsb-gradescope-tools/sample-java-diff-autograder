ebfb4c9ad75b61b938f220c51d94334a0436f567
Phill Conrad <pconrad@cs.ucsb.edu>
2019-09-25 15:50:36 -0700

    Update diffs.sh

--- diffs.sh
+++ diffs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#@test{"stdout":20, "stderr": 20}
+#@test{"stdout":20, "stderr": 20, "name":"There should be a file called Hello.java in the submission"}
 ls -1 Hello.java
 
 #@test{"stdout":5, "stderr": 15, "timeout": 10.0}
